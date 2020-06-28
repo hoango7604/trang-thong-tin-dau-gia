e8b189e1baad15fa7768a06159376cb3c53078ba
HỘI NGUYỄN VĂN <16520456@ms.uit.edu.vn>
2020-06-28 08:03:27 +0700

    create api call auction product by category

--- frontend/src/views/Auction.vue
+++ frontend/src/views/Auction.vue
@@ -7,23 +7,50 @@ export default {
   data() {
     return {
       auctions: [],
+      currentAuctionId: null,
+      categories: [],
     };
   },
 
-  async created() {
-    //get Auction ID hien tai ra truoc sau do moi goi api de get ra danh sach san pham theo danh muc
-    // const currentAuction = await this.$axios.get(`Auction/GetCurrentAuction`);
-    const currentAuction = 0;
+  methods: {
+    async fetchProductsByCategory(categoryId) {
+      const res = await this.$axios.get("Product/GetProductsByFilter", {
+        params: {
+          categoryId: categoryId,
+          auctionId: this.currentAuctionId,
+          MaxResultCount: 100,
+        },
+      });
+      const { success, result } = res.data;
+      if (success) {
+        this.auctions = result.items;
+        console.log("auctions", this.auctions);
+      }
+    },
 
-    const res = await this.$axios.get("Product/GetProductsByFilter", {
-      categoryId: currentAuction,
-      auctionId: this.$route.params.id,
-    });
-    const { success, result } = res.data;
-    if (success) {
-      this.auctions = result.items;
-      console.log("created -> auctions", this.auctions);
-    }
+    async getCurrentAuctionId() {
+      //get Auction ID hien tai ra truoc sau do moi goi api de get ra danh sach san pham theo danh muc
+      // const currentAuctionId = await this.$axios.get(`Auction/GetCurrentAuction`);
+      const currentAuctionId = 0;
+      this.currentAuctionId = currentAuctionId;
+    },
+
+    async fetchAllCategory() {
+      const res = await this.$axios.get("Category/GetCategorysByFilter");
+      const { success, result } = res.data;
+      if (success) {
+        this.categories = result.items;
+        console.log("categories", this.categories);
+      }
+    },
+  },
+
+  async created() {
+    const categoryId = this.$route.params.id;
+
+    await this.getCurrentAuctionId();
+    await this.fetchProductsByCategory(categoryId);
+    await this.fetchAllCategory();
   },
 };
 </script>
