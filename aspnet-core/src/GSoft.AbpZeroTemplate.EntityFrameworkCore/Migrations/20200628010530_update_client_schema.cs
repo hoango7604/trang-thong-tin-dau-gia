8bdeb3cb418074f23a4d6541e781a8626da9004c
HỘI NGUYỄN VĂN <16520456@ms.uit.edu.vn>
2020-06-28 08:08:40 +0700

    update migration

---
+++ aspnet-core/src/GSoft.AbpZeroTemplate.EntityFrameworkCore/Migrations/20200628010530_update_client_schema.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GSoft.AbpZeroTemplate.Migrations
+{
+    public partial class update_client_schema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Clients",
+                newName: "Address");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Clients",
+                newName: "Username");
+        }
+    }
+}
